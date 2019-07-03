32857141a7bd57ff0f713cb9949e8bf98cec866e
Cilas Amos <cilas.acms@gmail.com>
2019-07-03 21:17:08 +0000

    Atualizar codenation/codenation/urls.py

--- codenation/codenation/urls.py
+++ codenation/codenation/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import include, path
 from televendas import views
 
 urlpatterns = [
-    path("api/", include('televendas.urls'))
+    path('admin/', admin.site.urls),
+    path("", include('televendas.urls'))
 ]
+
